ccf992219f27aee06203b68947c7a6476ab372b9
anandversespace-debug <anandverse.space@gmail.com>
2026-04-22 20:28:22 +0530

    fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -11,6 +12,7 @@ export default defineConfig({
       generatedRouteTree: "./src/routeTree.gen.ts",
     }),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
